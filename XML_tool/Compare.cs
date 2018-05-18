6fa39bffb15361d357ba19b5fa23938a105d9976
DETOX-ANG <34696296+DETOX-ANG@users.noreply.github.com>
2018-05-18 18:39:12 -0300

    Add_Compare_Fixes
    Created Compare Class and performed some code fixes

---
+++ XML_tool/Compare.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_tool
+{
+    class Compare
+    {
+        HashSet<string> xml1 = new HashSet<string>();
+    }
+}
--- XML_tool/Reader.cs
+++ XML_tool/Reader.cs
@@ -8,13 +8,17 @@ namespace XML_tool
 {
     class Reader
     {
+        HashSet<string>[] XmlFile;
         
+        
+        
+
         public Reader(string path) {
 
             XmlReaderSettings settings = new XmlReaderSettings();
             settings.DtdProcessing = DtdProcessing.Parse;
             XmlReader reader = XmlReader.Create(path, settings);
-            ;
+            
             while (reader.Read())
             {
                 switch (reader.NodeType)
@@ -23,38 +27,6 @@ namespace XML_tool
                         break;
                     case XmlNodeType.Element:
                         Console.WriteLine(reader.Name);
-                        break;
-                    case XmlNodeType.Attribute:
-                        break;
-                    case XmlNodeType.Text:
-                        break;
-                    case XmlNodeType.CDATA:
-                        break;
-                    case XmlNodeType.EntityReference:
-                        break;
-                    case XmlNodeType.Entity:
-                        break;
-                    case XmlNodeType.ProcessingInstruction:
-                        break;
-                    case XmlNodeType.Comment:
-                        break;
-                    case XmlNodeType.Document:
-                        break;
-                    case XmlNodeType.DocumentType:
-                        break;
-                    case XmlNodeType.DocumentFragment:
-                        break;
-                    case XmlNodeType.Notation:
-                        break;
-                    case XmlNodeType.Whitespace:
-                        break;
-                    case XmlNodeType.SignificantWhitespace:
-                        break;
-                    case XmlNodeType.EndElement:
-                        break;
-                    case XmlNodeType.EndEntity:
-                        break;
-                    case XmlNodeType.XmlDeclaration:
                         break;
                     default:
                         break;
