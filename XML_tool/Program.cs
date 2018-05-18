c69d3599577d3c1e6fd5e5cda3df81acbd631930
DETOX-ANG <34696296+DETOX-ANG@users.noreply.github.com>
2018-05-18 17:31:12 -0300

    First_Commit
    First Version of Files

--- XML_tool/Program.cs
+++ XML_tool/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace XML_tool
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Reader reader = new Reader("C:\\Users\\ARocha2\\Documents\\Avalara\\310.xml");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ XML_tool/Reader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XML_tool
+{
+    class Reader
+    {
+        
+        public Reader(string path) {
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+            XmlReader reader = XmlReader.Create(path, settings);
+            ;
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.None:
+                        break;
+                    case XmlNodeType.Element:
+                        Console.WriteLine(reader.Name);
+                        break;
+                    case XmlNodeType.Attribute:
+                        break;
+                    case XmlNodeType.Text:
+                        break;
+                    case XmlNodeType.CDATA:
+                        break;
+                    case XmlNodeType.EntityReference:
+                        break;
+                    case XmlNodeType.Entity:
+                        break;
+                    case XmlNodeType.ProcessingInstruction:
+                        break;
+                    case XmlNodeType.Comment:
+                        break;
+                    case XmlNodeType.Document:
+                        break;
+                    case XmlNodeType.DocumentType:
+                        break;
+                    case XmlNodeType.DocumentFragment:
+                        break;
+                    case XmlNodeType.Notation:
+                        break;
+                    case XmlNodeType.Whitespace:
+                        break;
+                    case XmlNodeType.SignificantWhitespace:
+                        break;
+                    case XmlNodeType.EndElement:
+                        break;
+                    case XmlNodeType.EndEntity:
+                        break;
+                    case XmlNodeType.XmlDeclaration:
+                        break;
+                    default:
+                        break;
+                }
+                
+                
+            }
+        }
+
+
+
+    }
+}
